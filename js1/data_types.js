8b2b061e7c3279885e2bb5d4f802104c7faa865d
GithubRepoKat <kattrrina2206@gmail.com>
2022-12-17 19:19:37 +0300

    Типы данных

--- js1/data_types.js
+++ js1/data_types.js
@@ -53,18 +53,18 @@
     let word_1 = "велосипед";
     let word_2 = "полезен";
 
-    console.log(word_1 + " " + word_2);
+    console.log(word_1 + " " + word_2); // велосипед полезен
 
 // q 3.2
     let name = "Всеволод";
     let last_name = "\nСтарозубов";
 
-    console.log((name.trim() + last_name.trim()).length);
+    console.log((name.trim() + last_name.trim()).length); // 18
 
 // q 3.3
     let word = "велосипед";
 
-    console.log(word.toUpperCase()[0]);
+    console.log(word.toUpperCase()[0]); // В
 
 // q 3.4
     let value = 5.4;
@@ -73,26 +73,26 @@
     let value_2 = Math.floor(value);
     let value_3 = Math.round(value);
     let value_4 = Math.sqrt(value_1 + value_2 + value_3);
-        console.log(Math.trunc(value_4));
+        console.log(Math.trunc(value_4)); // 4
 
 // q 3.5
     let a = 2;
     let b = 4;
 
-    console.log((a * b)**2);
+    console.log((a * b)**2); // 64
 
 // q 3.6
     a = 3;
     b = 4;
 
-    console.log(Math.sqrt(a**2 + b**2));
+    console.log(Math.sqrt(a**2 + b**2)); // 5
 
 // q 3.7
     name = "Анатолий"; 
     last_name = "Максимов"; 
     let age = 32;
 
-    console.log(`Имя: "${obj.name}" Фамилия: '${obj.last_name}' Возраст: ${obj.age}`);
+    console.log(`Имя: "${obj.name}" Фамилия: '${obj.last_name}' Возраст: ${obj.age}`); // Имя: "Анатолий" Фамилия: 'Максимов' Возраст: 32
 
 // q 3.8
     name = "Анатолий"; 
@@ -100,7 +100,7 @@
     age = 32;
 
     let obj = {"name": name, "last_name": last_name, "age": age};
-        console.log(obj);
+        console.log(obj); // {name: "Анатолий", last_name: "Максимов", age: 32}
 
 // q 3.9
     value = "привет";
@@ -109,22 +109,22 @@
         console.log("null");
     } else {
         console.log(typeof value);
-    }
+    } // string
 
 // q 3.10
     let num_1 = "3";
     let num_2 = "5";
 
-    console.log(+num_1 + +num_2);
+    console.log(+num_1 + +num_2); // 8
 
 // q 3.11
     let size = "500.5%";
 
-    console.log(parseFloat(size));
+    console.log(parseFloat(size)); // 500.5
 
 // q 3.12
     a = "5px";
     b = "7px";
     let c = "10px";
 
-    console.log(parseFloat(a) * parseFloat(b) * parseFloat(c));
+    console.log(parseFloat(a) * parseFloat(b) * parseFloat(c)); // 350
